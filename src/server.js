dca43f2db91fb80a04a3d4a1d642cbd3149c1f64
Yuna Lee <carnivalss@naver.com>
2020-07-31 09:36:17 +0900

    import logger from "morgan"

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
 
 const PORT = process.env.PORT || 8800;
 
@@ -16,5 +17,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () => console.log(`🌏Server running on http://localhost:${PORT}`));
