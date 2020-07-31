7c3ddac9e2f99a8ce80bf39605a3f6b703025e94
Yuna Lee <carnivalss@naver.com>
2020-07-31 09:32:05 +0900

    Setting up

---
+++ src/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+
+const PORT = process.env.PORT || 8800;
+
+const typeDefs = `
+	type Query {
+		hello: String!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		hello: () => "Hi"
+	}
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start({ port: PORT }, () => console.log(`🌏Server running on http://localhost:${PORT}`));
