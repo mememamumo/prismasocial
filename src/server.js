b0dc6d8535d427d2245a45f464f8889ec9443c21
Yuna Lee <carnivalss@naver.com>
2020-08-08 06:05:14 +0900

    env.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env")});
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
