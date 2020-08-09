ae6e9712c0611eab4b52192e31d2deb51947510d
Yuna Lee <carnivalss@naver.com>
2020-08-09 09:22:53 +0900

    Post.js computed - isLiked

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -1,10 +1,26 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-	Post: {
-		files: ({ id }) => prisma.post({ id }).files(),
-		comments: ({ id }) => prisma.post({ id }).comments(),
-		user: ({ id }) => prisma.post({ id }).user(),
-		likes: ({ id }) => prisma.post({ id }).likes()
-	}
+  Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: { id }
+          }
+        ]
+      });
+    }
+  }
 };
