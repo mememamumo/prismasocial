48f0adb51a1abfcfed9f0f7f1111bddd33850094
Yuna Lee <carnivalss@naver.com>
2020-08-09 14:01:50 +0900

    API - seeFeed

---
+++ src/api/Post/seeFeed/seeFeed.js
@@ -0,0 +1,22 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeFeed: async (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const following = await prisma.user({ id: user.id }).following();
+      // // test
+      // console.log(following);
+      // console.log(following.map((user) => user.id));
+      return prisma.posts({
+        where: {
+          user: {
+            id_in: [...following.map((user) => user.id), user.id]
+          }
+        },
+        orderBy: "createdAt_DESC"
+      });
+    }
+  }
+};
