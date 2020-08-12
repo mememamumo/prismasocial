dad4f75a2ffe8fa259a146009a90cff681680ddf
Yuna Lee <carnivalss@naver.com>
2020-08-12 11:12:17 +0900

    Refactoring createAccount.js

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,17 +1,28 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-	Mutation: {
-		createAccount: async (_, args) => {
-			const { username, email, firstName="", lastName="", bio="" } = args;
-			const user = await prisma.createUser({
-				username,
-				email, 
-				firstName,
-				lastName,
-				bio
-			})
-			return user;
-		}
-	}
+  Mutation: {
+    createAccount: async (_, args) => {
+      const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            username
+          },
+          { email }
+        ]
+      });
+      if (exists) {
+        throw Error("사용자의 이름/ 이메일이 이미 존재합니다.");
+      }
+      await prisma.createUser({
+        username,
+        email,
+        firstName,
+        lastName,
+        bio
+      });
+      return true;
+    }
+  }
 };
