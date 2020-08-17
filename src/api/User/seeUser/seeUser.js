db6b05ff7311c2dce26f9ebcc006129c12783fcd
Yuna Lee <carnivalss@naver.com>
2020-08-17 15:34:49 +0900

    Refactoring - seeUser

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,10 +1,10 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-	Query: {
-		seeUser: async (_, args) => {
-			const { id } = args;
-			return prisma.user({ id });
-		}
-	}
+  Query: {
+    seeUser: async (_, args) => {
+      const { username } = args;
+      return prisma.user({ username });
+    }
+  }
 };
