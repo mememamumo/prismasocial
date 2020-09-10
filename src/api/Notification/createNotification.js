e31df20ed423db00545694feb83aea46e86035ee
Yuna Lee <carnivalss@naver.com>
2020-09-10 23:51:45 +0900

    #4 Refactoring - Explore (user)

--- src/api/Notification/createNotification.js
+++ src/api/Notification/createNotification.js
@@ -1,39 +1,39 @@
-import { prisma } from "../../../generated/prisma-client";
+// import { prisma } from "../../../generated/prisma-client";
 
-const createNotification = async (fromId, toId, noteType, post) => {
-  if (post === undefined) {
-    try {
-      if (fromId === toId) {
-        return false;
-      } else {
-        await prisma.createNotification({
-          from: { connect: { id: fromId } },
-          to: { connect: { id: toId } },
-          type: noteType
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  } else {
-    try {
-      if (fromId === toId) {
-        return false;
-      } else {
-        await prisma.createNotification({
-          from: { connect: { id: fromId } },
-          to: { connect: { id: toId } },
-          type: noteType,
-          post: { connect: { id: post } }
-        });
-      }
-      return false;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  }
-};
+// const createNotification = async (fromId, toId, noteType, post) => {
+//   if (post === undefined) {
+//     try {
+//       if (fromId === toId) {
+//         return false;
+//       } else {
+//         await prisma.createNotification({
+//           from: { connect: { id: fromId } },
+//           to: { connect: { id: toId } },
+//           type: noteType
+//         });
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       return false;
+//     }
+//   } else {
+//     try {
+//       if (fromId === toId) {
+//         return false;
+//       } else {
+//         await prisma.createNotification({
+//           from: { connect: { id: fromId } },
+//           to: { connect: { id: toId } },
+//           type: noteType,
+//           post: { connect: { id: post } }
+//         });
+//       }
+//       return false;
+//     } catch (error) {
+//       console.log(error);
+//       return false;
+//     }
+//   }
+// };
 
-export default createNotification;
+// export default createNotification;
