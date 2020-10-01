9b1ad1e581521f8797328035da0b533285b79898
Yuna Lee <carnivalss@naver.com>
2020-10-01 11:57:43 +0900

    #6 Refactoring seeUser and upload

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -4,7 +4,14 @@ export default {
   Query: {
     seeUser: async (_, args) => {
       const { username } = args;
-      return prisma.user({ username });
+      // return prisma.user({ username });
+      const exists = await prisma.$exists.user({ username });
+
+      if (exists) {
+        return prisma.user({ username });
+      } else {
+        throw Error("User not found...");
+      }
     }
   }
 };
