f27723673e57356f293b0bd059c907cf84d7a171
Yuna Lee <carnivalss@naver.com>
2020-10-10 02:37:03 +0900

    #7.1 Uploading to S3

--- src/upload.js
+++ src/upload.js
@@ -1,12 +1,35 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-2"
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "prismasocial",
+    metadata: function (req, file, cb) {
+      cb(null, { fileName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path }
+    file: { location }
   } = req;
-  console.log(path);
-  res.json({ path });
+  console.log(location);
+  res.json({ location });
 };
